54306515b7c695fb3d6423acb43dae49349742b1
unknown <mpangoni@WSPDBB33750.corporate.int>
2013-09-26 11:54:36 -0300

    Issue #1 Correction

--- lib/LL_rule.rb
+++ lib/LL_rule.rb
@@ -13,13 +13,16 @@ module LL
     end
     
     def push(stack)
+      
       @stack.reverse.each do |v| 
         
         if !v.kind_of?(Symbol) and @handler then
           $LOGGER.debug("Adding Handler #{@handler} to symbol #{v}")
+                    
+          v.instance_variable_set(:@handler, @handler) 
           
           def v.fire(token)
-            $LOGGER.debug("Executing #{@handler} to symbol #{v} with token #{token}")
+            $LOGGER.debug("Executing #{@handler} to symbol #{self} with token #{token}")
             @handler.call(token)
           end
         end
